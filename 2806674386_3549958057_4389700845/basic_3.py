2a6db8846c6f893e248c4db062c61beb210d1549
TrojanHrse PR <29535782+trojanhrsepr@users.noreply.github.com>
2022-05-09 19:42:18 -0700

    Fin.

--- 2806674386_3549958057_4389700845/basic_3.py
+++ 2806674386_3549958057_4389700845/basic_3.py
@@ -1,6 +1,42 @@
-import helper_3 as helper
 import sys
 import time
+import psutil
+import os
+import gc
+
+class helper_3:
+    # Gap Penalty constant
+    gap_penalty = 30
+
+    # String substitution dictionary
+    cost_dict = {"AC": 110, "AG": 48, "AT": 94, "CG": 118, "CT": 48, "GT": 110, "AA": 0, "CC": 0, "GG": 0, "TT": 0,
+                "CA":110, "GA": 48, "TA": 94, "GC": 118, "TC": 48, "TG": 110}
+
+    # Memory utilization
+    def process_memory(self):                                
+        process = psutil.Process(os.getpid())
+        memory_info = process.memory_info()
+        memory_consumed = int(memory_info.rss/1000)
+        return memory_consumed
+
+    # Calculates time taken and memory utilization
+    def calculate_time_mem(self, start_time):
+        mem = self.process_memory()                          
+        end_time = time.time()
+        time_taken = (end_time - start_time)*1000
+        print("Time taken for algorithm: " + str(time_taken) + "ms")
+        print("Memory usage by algorithm: " + str(mem) + "KB")
+        return time_taken, mem
+
+    # Creates output file
+    def create_output_file(self, filename, start_time, cost, X, Y, time_taken, mem):
+        with open(filename, 'w') as fp:       # to write to file
+            fp.write(str(cost) +"\n")
+            fp.write(X + "\n")
+            fp.write(Y + "\n")
+            fp.write(f"{time_taken}\n")
+            fp.write(f"{mem}\n")
+
 
 def basic_algo(filename, gap_penalty, cost_dict):
     with open(filename) as fp:                                  # load file
@@ -69,11 +105,12 @@ def basic_algo(filename, gap_penalty, cost_dict):
 
     return OPT[lenX - 1][lenY - 1], "".join(X_new), "".join(Y_new)
 
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python3 <filename.py> <input.txt>")
         sys.exit()
+
+    helper = helper_3()
 
     start_time = time.time()
     cost, X, Y = basic_algo(sys.argv[1], helper.gap_penalty, helper.cost_dict)
--- 2806674386_3549958057_4389700845/efficient_3.py
+++ 2806674386_3549958057_4389700845/efficient_3.py
@@ -1,6 +1,41 @@
-import helper_3 as helper
 import sys
 import time
+import psutil
+import os
+import gc
+
+class helper_3:
+  # Gap Penalty constant
+  gap_penalty = 30
+
+  # String substitution dictionary
+  cost_dict = {"AC": 110, "AG": 48, "AT": 94, "CG": 118, "CT": 48, "GT": 110, "AA": 0, "CC": 0, "GG": 0, "TT": 0,
+                "CA":110, "GA": 48, "TA": 94, "GC": 118, "TC": 48, "TG": 110}
+
+  # Memory utilization
+  def process_memory(self):                                
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    memory_consumed = int(memory_info.rss/1000)
+    return memory_consumed
+
+  # Calculates time taken and memory utilization
+  def calculate_time_mem(self, start_time):
+    mem = self.process_memory()                          
+    end_time = time.time()
+    time_taken = (end_time - start_time)*1000
+    print("Time taken for algorithm: " + str(time_taken) + "ms")
+    print("Memory usage by algorithm: " + str(mem) + "KB")
+    return time_taken, mem
+
+  # Creates output file
+  def create_output_file(self, filename, start_time, cost, X, Y, time_taken, mem):
+    with open(filename, 'w') as fp:       # to write to file
+      fp.write(str(cost) +"\n")
+      fp.write(X + "\n")
+      fp.write(Y + "\n")
+      fp.write(f"{time_taken}\n")
+      fp.write(f"{mem}\n")
 
 def get_input(file_name):
   with open(file_name) as fp: # load file
@@ -134,12 +169,14 @@ def space_efficient(filename):
   X,Y = get_input(filename)
   str1,str2 = DC(X,Y)
   cost = get_cost(str1,str2)
-  return cost, str1, str2
+  return cost, str1, str2  
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python3 <filename.py> <input.txt>")
         sys.exit()
+
+    helper = helper_3()
 
     l = []
     start_time = time.time()
